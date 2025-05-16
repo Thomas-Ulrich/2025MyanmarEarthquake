ae5fe216b04276da642022343c62d206c3e962af
Thomas-Ulrich <ulrich@geophysik.uni-muenchen.de>
2025-05-16 11:02:52 +0200

    improve downweight_bad_surface_waves.py

--- inversion/downweight_bad_surface_waves.py
+++ inversion/downweight_bad_surface_waves.py
@@ -1,32 +1,32 @@
 import pandas as pd
 import json
 
-# Read the file, assuming it is space-separated and has variable-width columns
 df = pd.read_csv("misfit_details.txt", sep="\s+")
 df["w_misfit"] = df["weight"] * df["misfit"]
-
-df = df[df["component"].isin(["L", "R"])]
-percentile_80 = df["w_misfit"].quantile(0.80)
-print("80th percentile w_misfit:", percentile_80)
-
 df_sorted = df.sort_values(by="w_misfit", ascending=False)
-
-# Load the JSON data
-with open("surf_waves.json") as f:
-    surf_wave = json.load(f)
-
-# Optional: strip and upper-case names to avoid mismatches
-misfit_map = df.set_index("sta_name")["w_misfit"].to_dict()
-
-for entry in surf_wave:
-    station_name = entry["name"]
-    misfit = misfit_map.get(station_name)
-    if misfit is not None and misfit > percentile_80:
-        entry["trace_weight"] = 0.0
-
-with open("surf_waves.json", "w") as f:
-    json.dump(surf_wave, f, indent=4)
-
-
 print(df_sorted)
 df_sorted.to_csv("misfit_sorted.csv", index=False)
+
+
+df = df[df["component"].isin(["L", "R"])]
+percentile_50 = df["misfit"].quantile(0.50)
+
+threshold = 1.6
+n_downweight = (df["misfit"] > threshold * percentile_50).sum()
+print("signals to be downweighted: ", n_downweight)
+print(f"out of {len(df)} ({n_downweight/len(df)*100:.1f}%)")
+if n_downweight:
+
+    with open("surf_waves.json") as f:
+        surf_wave = json.load(f)
+
+    for entry in surf_wave:
+        station_name = entry["name"]
+        component = "R" if entry["component"] == "BHZ" else "L"
+        row = df[(df["sta_name"] == station_name) & (df["component"] == component)]
+        if row["misfit"].values[0] > threshold * percentile_50:
+            entry["trace_weight"] = 0.0
+            print(row)
+
+    with open("surf_waves.json", "w") as f:
+        json.dump(surf_wave, f, indent=4)
